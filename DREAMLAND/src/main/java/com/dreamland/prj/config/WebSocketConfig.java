39de90613546cf448147a67128bb3542818ba40e
soyi9601 <soyi9601@gmail.com>
2024-06-12 14:00:45 +0900

    KFIM-56 소이-다시커밋..

--- DREAMLAND/src/main/java/com/dreamland/prj/config/WebSocketConfig.java
+++ DREAMLAND/src/main/java/com/dreamland/prj/config/WebSocketConfig.java
@@ -1,37 +1,37 @@
-/*
- * package com.dreamland.prj.config;
- * 
- * import org.springframework.context.annotation.Configuration; import
- * org.springframework.messaging.simp.config.ChannelRegistration; import
- * org.springframework.messaging.simp.config.MessageBrokerRegistry; import
- * org.springframework.web.socket.config.annotation.
- * EnableWebSocketMessageBroker; import
- * org.springframework.web.socket.config.annotation.StompEndpointRegistry;
- * import org.springframework.web.socket.config.annotation.
- * WebSocketMessageBrokerConfigurer;
- * 
- * import lombok.RequiredArgsConstructor;
- * 
- * @Configuration
- * 
- * @EnableWebSocketMessageBroker
- * 
- * @RequiredArgsConstructor public class WebSocketConfig implements
- * WebSocketMessageBrokerConfigurer {
- * 
- * private final StompHandler stompHandler; // jwt 토큰 인증 핸들러
- * 
- * @Override public void registerStompEndpoints(StompEndpointRegistry registry)
- * { registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS(); }
- * 
- * 
- * @Override public void configureMessageBroker(MessageBrokerRegistry registry)
- * { registry.enableSimpleBroker("/topic");
- * registry.setApplicationDestinationPrefixes("/app"); }
- * 
- * @Override public void configureClientInboundChannel(ChannelRegistration
- * registration) { registration.interceptors(stompHandler);//핸들러 등록 }
- * 
- * 
- * }
- */
+package com.dreamland.prj.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  private final StompHandler stompHandler; // jwt 토큰 인증 핸들러
+  
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+  }
+  
+  
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.enableSimpleBroker("/topic");
+    registry.setApplicationDestinationPrefixes("/app");
+  }
+  
+  @Override
+  public void configureClientInboundChannel(ChannelRegistration registration) {
+      registration.interceptors(stompHandler);//핸들러 등록
+  }
+  
+  
+}
